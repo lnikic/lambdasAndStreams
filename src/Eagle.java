be983e83ae237d7c1a09b7ed5a938deaee6c5cc4
nikic <nikic.luka2001@gmail.com>
2025-12-22 19:27:41 +0100

    lambdas examples and types of FI

--- src/Eagle.java
+++ src/Eagle.java
@@ -1,7 +1,14 @@
-public class Eagle implements Bird{
+import java.util.Comparator;
+
+public class Eagle implements Bird, Comparator<Eagle> {
 
     @Override
     public void canFly(String val) {
         System.out.println("Eagle Bird Implementation");
+    }
+
+    @Override
+    public int compare(Eagle o1, Eagle o2) {
+        return 0;
     }
 }
